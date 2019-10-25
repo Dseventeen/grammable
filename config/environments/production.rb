3609ad9afc0574d0fae2f5eb3a8cf1e28c57abdb
Dan <dseventeen@gmail.com>
2019-10-25 18:42:46 +0000

    added devise and posting of grams

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'grammable-dan-vega.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
